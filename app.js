3116b48058ef1bd72b8623be7f70220137f1e34a
mirziyob1 <mrziyo12@gmail.com>
2025-11-19 12:52:48 +0500

    Uy ishi bajarildi

---
+++ app.js
@@ -0,0 +1,35 @@
+// let car={brand:"BMW",model:"X5",color:"back"}
+// delete car.model
+// console.log(car)
+
+//13
+// let users=[
+//     {name:"Ali",age:22},
+//     {name:"Ali2",age:28},
+//     {name:"Ali3",age:19}
+// ]
+// let user=users.filter(name=>name.age<25)
+// console.log(user)
+
+//14
+// let prices={apple:5000,banana:7000,orange:6000}
+// let summ=Object.values(prices).reduce((ass,b)=>ass+b,0)
+// console.log(summ)
+
+//16
+// let student={name:"Ali",group:"JS-101"}
+// let student1=student
+// console.log(student1)
+
+//17
+// let company={
+//     name:"Techsoft",
+//     location:{
+//         city:"Toshkent",
+//         country:"Uzbekistan"
+//     }
+// }
+// company.location.city="Namangan"
+// console.log(company)
+
+//if-
---
+++ if.js
@@ -0,0 +1,126 @@
+//1
+// let username=prompt("Username")
+// let password=prompt("Pasword")
+// if (username==='admin' && password==="12345"){
+//     console.log("Xush kelibsiz Admin")
+// }
+// else if (username==="user" && password==='12345'){
+//     console.log("Salom,oddiy foydalanuvchi")
+// }
+// else{
+//     console.log("Login yoki parol notogri")
+// }
+
+//2
+// let yosh=+prompt("Yosh")
+// let tj=+prompt("Tajriba yili")
+// if (yosh<18){
+//     console.log("Sugurta olinmaydi")
+// }
+// else if(yosh<=25 && yosh>18 && tj<2){
+//     console.log("$300")
+// }
+// else if(yosh<=25 && yosh>18 && tj>=2){
+//     console.log("$200")
+// }
+// else if(yosh>25){
+//     console.log("$150")
+// }
+
+//3
+// let price=+prompt("Mahsulot narxini kiriting:")
+// let coupon=prompt("Copun nomini kiriting:")
+// if (coupon=="SALE20"){
+//     console.log("20% chegirma")
+// }
+// else if(coupon=="SALE50"){
+//     console.log("50% chegirma")
+// }
+// else{
+//     console.log("coupon yaroqsiz")
+// }
+
+//4
+// let hour=+prompt("Soat nechiligini kiriting:")
+// if (hour>=6 && hour<=20){
+//     console.log("Night mode ON")
+// }
+// else{
+//     console.log("Day mode ON")
+// }
+
+
+//5
+// let expyear=+prompt("Yaroq qilish mudatini kiriting:")
+// if(expyear<2025){
+//     console.log("Karta muddati o'tgan")
+// }
+// else{
+//     console.log("Karta amal qiladi")
+// }
+
+//6
+// let ordtype=+prompt("Buyurtma turini kiriting:")
+// if (ordtype=='delivery'){
+//     console.log("Kuryer yo'lda")
+// }
+// else if(ordtype=='pickup'){
+//     console.log("Buyurtmani olib ketishingiz mumkin")
+// }
+// else if(ordtype=='deni-in'){
+//     console.log("Stol tayyor")
+// }
+
+//7
+// let bal=+prompt("Balingizni kiriting:")
+// if (bal>=90){
+//     console.log("A")
+// }
+// else if (bal<90 && bal>=80){
+//     console.log("B")
+// }
+// else if (bal<80 && bal>=70){
+//     console.log("C")
+// }
+// else if (bal<70 && bal>=60){
+//     console.log("D")
+// }
+// else if (bal<60){
+//     console.log("F")
+// }
+
+//8
+// let temp=+prompt("Havo gradusini kiriting:")
+// if (temp==30){
+//     console.log("Yangil kiyim kiying")
+// }
+// else if (temp<=29 && temp>=15){
+//     console.log("Oddiy kiyim")
+// }
+// else if(temp<15){
+//     console.log("Issiq kiyim kiying")
+// }
+
+//9
+// let stock=+prompt("Mahsulot sonini kiriting:")
+// if (stock>10){
+//     console.log("Mahsulot yetarli")
+// }
+// else if(stock<=10 && stock>=1){
+//     console.log("Mahsulot yetishmaydi")
+// }
+// else{
+//     console.log("Sotuvda yo'q")
+// }
+
+//10
+// let intgb=+prompt("Internet tarifini kiriting:")
+// if(intgb<5){
+//     console.log("Mini tarif")
+// }
+// else if(intgb>=5 && intgb<20){
+//     console.log("Standart tarif")
+// }
+// else if(intgb>=20){
+//     console.log("Mage tarif")
+// }
